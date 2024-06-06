818b7877d7c9730eae9e2e35f310214e09ef84b3
mdrobinson <mdrobinson0124@yahoo.com>
2024-06-06 17:29:51 -0500

    wipe

--- src/routes/schedule.ts
+++
@@ -1,7 +0,0 @@
-import client from '../sanity/sanityClient';
-import type { Games } from '../types/game.js';
-
-export async function get() {
-	const games: Games = await client.fetch(`*[_type == "game" && !(_id in path('drafts.**'))]`);
-	return { body: { games } };
-}
--- src/types/sponsor.ts
+++
@@ -1,9 +0,0 @@
-export type Sponsor = {
-	sponsorDescription: string;
-	sponsorName: string;
-	sponsorUrl: string;
-	sponsorLogo: string;
-	displayOrder: number;
-};
-
-export type Sponsors = [sponsor: Sponsor];
